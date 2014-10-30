497d46d81422fa08b8c61b598ec7d90b6de9db67
Hanna Mendes Levitin <h.mendes.levitin@gmail.com>
2014-10-30 15:15:59 -0400

    refactored out blast version into own variable to be set later in config

--- tools/blast.py
+++ tools/blast.py
@@ -2,8 +2,13 @@
 import tools
 import os
 
-urlPrefix = 'ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/' \
-                + 'ncbi-blast-2.2.29+-'
+# TODO: specify version in config file
+VERSION="2.2.29+"
+
+URL_BASE = ''.join([
+    'ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-',
+    VERSION
+])
 
 def get_url() :
     uname = os.uname()
@@ -16,17 +21,25 @@ def get_url() :
             osStr = 'ia32-linux'
     else :
         raise NotImplementedError('OS {} not implemented'.format(uname[0]))
-    return urlPrefix + osStr + '.tar.gz'
+    return '{}-{}.tar.gz'.format(URL_BASE, osStr)
+
 
 class BlastTools(tools.Tool) :
-    """'Abstract' base class for tools in the blast+ suite.
-       Subclasses must define class member subtoolName."""
+    """
+    Base class for tools in the blast+ suite.
+       Subclasses must define class member subtoolName.
+
+    Previous note said that this was an 'Abstract' base class.  If we want
+    that, we have to make the class abstract though...
+    """
     def __init__(self, install_methods = None) :
         if install_methods == None :
-            target_rel_path = 'ncbi-blast-2.2.29+/bin/' + self.subtoolName
             url = get_url()
+            target_rel_path = 'ncbi-{}/bin/{}'.format(VERSION,
+                    self.subtoolName)
             install_methods = [tools.DownloadPackage(url, target_rel_path)]
         tools.Tool.__init__(self, install_methods = install_methods)
+
 
 class BlastnTool(BlastTools) :
     subtoolName = 'blastn'
